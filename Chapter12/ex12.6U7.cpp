976eb26da558bd8a359724c156ef739a19410b46
Bohaixileiyin <shockerfi@foxmail.com>
2020-07-10 15:13:54 +0800

    7.10

--- Chapter12/ex12.6U7.cpp
+++ Chapter12/ex12.6U7.cpp
@@ -39,17 +39,19 @@ void swirte(shared_ptr<vector<int>> p){
 int func(){}
 
 int main(){
-    // auto pointer = vec();
-    // read(pointer);
-    // wirte(pointer);
-    // delete pointer;
+    auto pointer = vec();
+    read(pointer);
+    wirte(pointer);
+    delete pointer;
 
-    // cout << cin.eof() << endl;//返回1说明在子函数eof后，状态是保持整个进程
-    // cin.clear();
+    cout << cin.eof() << endl;//返回1说明在子函数eof后，状态是保持整个进程
+    cin.clear();
 
-    // auto spointer = svec();
-    // sread(spointer);
-    // swirte(spointer);
+    auto spointer = svec();
+    sread(spointer);
+    swirte(spointer);
+
+    cout << "************************" << endl;
 
     int i();//这是函数声明！！！而非值初始化
     cout << i << endl;//输出是1注意别被误导，i不是变量是函数
@@ -57,6 +59,7 @@ int main(){
 
     int j{};//这才是值初始化
     cout << j << endl;//内置类型没有构造函数，所以值初始化都是零初始化
+    //详见stackflow         https://stackoverflow.com/questions/17131911/what-does-int-do-in-c
 
     int* pi = new int();
     cout << *pi << endl;
