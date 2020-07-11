0f57aab235e069b38c00c4a4f6d765b37fed936d
Bohaixileiyin <shockerfi@foxmail.com>
2020-07-11 22:22:27 +0800

    修复了查询不存在的单词7.11

--- Chapter12/myQueryResult.hpp
+++ Chapter12/myQueryResult.hpp
@@ -18,11 +18,14 @@ using std::endl;
 using std::ostream;
 
 class myQueryResult{
+    friend ostream &print(ostream &os,const myQueryResult &res);
     public:
         myQueryResult() = default;
         myQueryResult(const string w,shared_ptr<vector<string>> txt,shared_ptr<map<string,set<int>>> ln):word(w),text(txt),lineNumber(ln){}
 
-        void printWord()const{cout << word << " occurs " << (*(*lineNumber).find(word)).second.size() << "  times" << endl;}
+        void printWord()const{
+            cout << word << " occurs " << (*(*lineNumber).find(word)).second.size() << "  times" << endl;
+        }
         void printText()const{
             for(auto &ln:(*(*lineNumber).find(word)).second){
                 cout << "    " << "(line " << ln << ")" << text->at(ln - 1) << endl;
@@ -34,8 +37,17 @@ class myQueryResult{
         shared_ptr<map<string,set<int>>> lineNumber = std::make_shared<map<string,set<int>>>();
 };
 ostream &print(ostream &os,const myQueryResult &res){
-    res.printWord();
-    res.printText();
+    // res.printWord();
+    // res.printText();
+    auto iter = (*res.lineNumber).find(res.word);
+    if(iter == (*res.lineNumber).end())
+        cout << res.word << " occurs " << 0 << "  times" << endl;
+    else{
+        cout << res.word << " occurs " << (*iter).second.size() << "  times" << endl;
+        for(auto &ln:(*iter).second){
+            cout << "    " << "(line " << ln << ")" << res.text->at(ln - 1) << endl;
+        }
+    }
     return os;
 }
 
